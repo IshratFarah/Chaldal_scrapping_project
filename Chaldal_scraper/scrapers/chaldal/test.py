7fda28ebed17d8ef90b17f9e0e406b7ac5af9389
IshratFarah <ishrat.tanzila.farah@gmail.com>
2024-03-25 00:31:40 +0600

    trying to generalize the scrap_products function for fetching submenu info as well. incomplete code, not executed.

--- Chaldal_scraper/scrapers/chaldal/test.py
+++ Chaldal_scraper/scrapers/chaldal/test.py
@@ -23,6 +23,8 @@ category_url_list = []  # contains the links to the categories in sidebars
 categories = []  # contains the titles of the categories in sidebars
 subcategory_url_list = []   # contains the links to the subcategories
 subcategory = []    # contains the titiles to the subcategories
+df_submenu = pd.DataFrame()
+
 
 def find_links(link_container):
     container = link_container
@@ -67,34 +69,47 @@ def scroll_page():
         last_height = new_height
 
 
-def scrap_product_info(products, menubar):
+def scrap_product_info(soup,menubar):
     i = 0
-    for product in products:
-        if i == 2:
-            i = 0
-            break
-        item = product.find("div", class_="name").text
-        quantity = product.find("div", class_="subText").text
-        if product.find("div", class_="discountedPrice"):
-            price = product.find("div", class_="discountedPrice").text
-        else:
-            price = product.find("div", class_="price").text
-        product_link = product.find("a", class_="btnShowDetails").get("href")
-        items.append(item)
-        quantities.append(quantity)
-        prices.append(price)
-        product_links.append(product_link)
-        description = find_product_detail(product_link)
-        descriptions.append(description)
-        category.append(menubar)
-        i = i + 1
-        print(i)
+    try:
+        productPane = soup.find("div", class_="productPane")
+        products = productPane.findAll("div", class_="product")  # prod_elements == products
+        for product in products:
+            if i == 2:
+                i = 0
+                break
+            item = product.find("div", class_="name").text
+            quantity = product.find("div", class_="subText").text
+            if product.find("div", class_="discountedPrice"):
+                price = product.find("div", class_="discountedPrice").text
+            else:
+                price = product.find("div", class_="price").text
+            product_link = product.find("a", class_="btnShowDetails").get("href")
+            items.append(item)
+            quantities.append(quantity)
+            prices.append(price)
+            product_links.append(product_link)
+            description = find_product_detail(product_link)
+            descriptions.append(description)
+            category.append(menubar)
+            i = i + 1
+            print(i)
+    except  AttributeError:
+        links = []
+        titles = []
+        sublink_container = driver.find_elements(By.CLASS_NAME, "category-links-wrapper")
+        links, titles = find_links(sublink_container)
+        data_s = {'menu': titles, 'url': links}
+        df_s = pd.DataFrame(data_s)
+        df_s.set_index('url', inplace=True)
+        print(df_s)
+        df_submenu = pd.concat([df_submenu, df_s], ignore_index=True)
 
 
 def main():
     link_container = driver.find_elements(By.CSS_SELECTOR, "ul[class*='level-']")
     category_url_list, categories = find_links(link_container)
-    df_submenu = pd.DataFrame()
+    # df_submenu = pd.DataFrame()
 
     # Save the sidebars and associated links
     data = {'menu': categories, 'url': category_url_list}
@@ -106,21 +121,7 @@ def main():
     for url in category_url_list:
         soup = initialize(url)
         menubar = df.loc[url, 'menu']
-        try:
-            print(menubar)
-            productPane = soup.find("div", class_="productPane")
-            prod_elements = productPane.findAll("div", class_="product")
-            scrap_product_info(prod_elements, menubar)
-        except AttributeError:
-            links = []
-            titles = []
-            sublink_container = driver.find_elements(By.CLASS_NAME, "category-links-wrapper")
-            links, titles = find_links(sublink_container)
-            data_s = {'menu': titles, 'url': links}
-            df_s = pd.DataFrame(data_s)
-            df_s.set_index('url', inplace=True)
-            print(df_s)
-            df_submenu = pd.concat([df_submenu, df_s], ignore_index=True)
+        scrap_product_info(soup,menubar)
 
     # with pd.ExcelWriter(
     #         "E:\Projects\Chaldal_scrapping_project\Chaldal_scraper\scrapped_data\Chaldaal_dynamic_test.xlsx",
