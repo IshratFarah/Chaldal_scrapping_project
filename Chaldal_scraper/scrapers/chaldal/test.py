3e234ae6f0eee2d38c1d94e6262fcb089895ac48
IshratFarah <ishrat.tanzila.farah@gmail.com>
2024-03-25 00:04:59 +0600

    subcategory scrapped. product information of subcategories not scrapped yet.

--- Chaldal_scraper/scrapers/chaldal/test.py
+++ Chaldal_scraper/scrapers/chaldal/test.py
@@ -5,31 +5,38 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
-# Initialize the WebDriver
+
+
+# Initialize webdriver
 driver = webdriver.Chrome()
+url_home = "https://chaldal.com"
+driver.get(url_home)
 
-# Open the webpage
-URL_home = "https://chaldal.com"
-driver.get(URL_home)
-
-# Define variables
-categories = []   # contains the names of the sidebars
-category_url_list = []   # contains the links to the sidebars
-subcategory_url_list = []
-subcategory = []
+# Define global variables
+items = []
+quantities = []
+prices = []
+product_links = []
+descriptions = []
+category = []
+category_url_list = []  # contains the links to the categories in sidebars
+categories = []  # contains the titles of the categories in sidebars
+subcategory_url_list = []   # contains the links to the subcategories
+subcategory = []    # contains the titiles to the subcategories
 
 def find_links(link_container):
     container = link_container
     url_list = []
     url_title = []
-    print("find_links function entered")
-    print(container)
+    # print("find_links function entered")
+    # print(container)
     for a_tags in container:
-        links = a_tags.find_elements(By.TAG_NAME, "a")   # finds the <a> tags
+        links = a_tags.find_elements(By.TAG_NAME, "a")
+        print(links)# finds the <a> tags
         for link in links:
             url_list.append(link.get_attribute("href"))    # finds the href from the <a> tags
             url_title.append(link.text)   # finds the texts shown associated with the links (options in the menus)
-            # print('option: ' + link.text + ' url: ' + url_sidebar)
+            print(url_title)
     return url_list, url_title
 
 
@@ -42,7 +49,7 @@ def initialize(url):
 
 
 def find_product_detail(product_link):
-    detail_link = URL_home + product_link
+    detail_link = url_home + product_link
     soup = initialize(detail_link)
     description = soup.find("div", class_="details").text
     return description
@@ -60,29 +67,10 @@ def scroll_page():
         last_height = new_height
 
 
-link_container = driver.find_elements(By.CSS_SELECTOR, "ul[class*='level-']")
-category_url_list, categories = find_links(link_container)
-
-# Save the sidebars and associated links
-data = {'menu': categories, 'url': category_url_list}
-df = pd.DataFrame(data)
-df.set_index('url', inplace=True)
-df.to_excel("E:\Projects\Chaldal_scrapping_project\Chaldal_scraper\scrapped_data\Chaldaal_dynamic_test.xlsx",
-            sheet_name="urls")
-
-items = []
-quantities = []
-prices = []
-product_links = []
-descriptions = []
-category = []
-# i = 0
-
-
-def scrap_product_info(products):
+def scrap_product_info(products, menubar):
     i = 0
     for product in products:
-        if i == 3:
+        if i == 2:
             i = 0
             break
         item = product.find("div", class_="name").text
@@ -102,69 +90,54 @@ def scrap_product_info(products):
         i = i + 1
         print(i)
 
-for url in category_url_list:
-    soup = initialize(url)
-    menubar = df.loc[url, 'menu']
-    try:
-        print(menubar)
-        productPane = soup.find("div", class_="productPane")
-        prod_elements = productPane.findAll("div", class_="product")
-        scrap_product_info(prod_elements)
-        # for product in products:
-        #     # print("entered loop")
-        #     if i == 3:
-        #         i = 0
-        #         break
-        #     item = product.find("div", class_="name").text
-        #     quantity = product.find("div", class_="subText").text
-        #     if product.find("div", class_="discountedPrice"):
-        #         price = product.find("div", class_="discountedPrice").text
-        #     else:
-        #         price = product.find("div", class_="price").text
-        #     product_link = product.find("a", class_="btnShowDetails").get("href")
-        #     items.append(item)
-        #     quantities.append(quantity)
-        #     prices.append(price)
-        #     product_links.append(product_link)
-        #     description = find_product_detail(product_link)
-        #     descriptions.append(description)
-        #     category.append(menubar)
-        #     i = i + 1
-        #     print(i)
-    except AttributeError:
-        # print("Exception occurred")
-        sublink_container = driver.find_elements(By.CLASS_NAME, "category-links-wrapper")
-        subcategory_url_list, subcategory = find_links(sublink_container)
-        # print(subcategory)
+
+def main():
+    link_container = driver.find_elements(By.CSS_SELECTOR, "ul[class*='level-']")
+    category_url_list, categories = find_links(link_container)
+    df_submenu = pd.DataFrame()
+
+    # Save the sidebars and associated links
+    data = {'menu': categories, 'url': category_url_list}
+    df = pd.DataFrame(data)
+    df.set_index('url', inplace=True)
+    df.to_excel("E:\Projects\Chaldal_scrapping_project\Chaldal_scraper\scrapped_data\Chaldaal_dynamic_test.xlsx",
+                sheet_name="urls")
+
+    for url in category_url_list:
+        soup = initialize(url)
+        menubar = df.loc[url, 'menu']
+        try:
+            print(menubar)
+            productPane = soup.find("div", class_="productPane")
+            prod_elements = productPane.findAll("div", class_="product")
+            scrap_product_info(prod_elements, menubar)
+        except AttributeError:
+            links = []
+            titles = []
+            sublink_container = driver.find_elements(By.CLASS_NAME, "category-links-wrapper")
+            links, titles = find_links(sublink_container)
+            data_s = {'menu': titles, 'url': links}
+            df_s = pd.DataFrame(data_s)
+            df_s.set_index('url', inplace=True)
+            print(df_s)
+            df_submenu = pd.concat([df_submenu, df_s], ignore_index=True)
+
+    # with pd.ExcelWriter(
+    #         "E:\Projects\Chaldal_scrapping_project\Chaldal_scraper\scrapped_data\Chaldaal_dynamic_test.xlsx",
+    #         engine='openpyxl', mode='a') as writer:
+    #     df_s.to_excel(writer, sheet_name="submenu_urls")
+    print(df_submenu)
+
+    # df2 = pd.DataFrame(columns=['item', 'quantity', 'price', 'description', 'category'],
+    #                    data={"item": items, "quantity": quantities, "price": prices,
+    #                    "description": descriptions, "category": category})
+    #
+    # with pd.ExcelWriter("E:\Projects\Chaldal_scrapping_project\Chaldal_scraper\scrapped_data\Chaldaal_dynamic_test.xlsx",
+    #                     engine='openpyxl', mode='a') as writer:
+    #     df2.to_excel(writer, sheet_name="raw_data")
+    #
+    # print(df2)
+    driver.close()
 
 
-# df2 = pd.DataFrame(columns=['item', 'quantity', 'price', 'description', 'category'],
-#                    data={"item": items, "quantity": quantities, "price": prices,
-#                    "description": descriptions, "category": category})
-#
-# with pd.ExcelWriter("E:\Projects\Chaldal_scrapping_project\Chaldal_scraper\scrapped_data\Chaldaal_dynamic_test.xlsx",
-#                     engine='openpyxl', mode='a') as writer:
-#     df2.to_excel(writer, sheet_name="raw_data")
-#
-# print(df2)
-driver.close()
-
-
-#### skip this part for now #####
-
-# food_sublinks = driver.find_elements(By.CSS_SELECTOR,"ul[class*='level-2']>li>div>a")
-# print(food_sublinks)
-# for sublink in food_sublinks:
-#     print(sublink.text.strip())
-# url_list.append(sublink.get_attribute("href"))
-
-# # def find_all_links(element):
-# #     child_elements = element.find_elements(By.XPATH,'//a')
-# #     if child_elements:
-# #         print('element has child')
-# #         for child_element in child_elements:
-# #             sub_links = child_element.find_elements(By.TAG_NAME,"a")
-# #             url_list.append([link.get_attribute("href") for link in sub_links])
-
-
-################################
+main()
