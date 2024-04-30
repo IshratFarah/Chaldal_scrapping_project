cb26d6ba23f1712468eceb28cc366b5250ef13e3
IshratFarah <ishrat.tanzila.farah@gmail.com>
2024-04-30 16:50:48 +0600

    menu scrapping with 2 items (for test purpose) done. submenu scrapping not working yet.

--- Chaldal_scraper/scrapers/chaldal/test_oop.py
+++ Chaldal_scraper/scrapers/chaldal/test_oop.py
@@ -5,17 +5,19 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
+url_home = "https://chaldal.com"
 submenu_list = []   # list of dataframes. Each df contain subcategories and associated links
 all_item = []   # contains the list of all item information
 path = "E:\\Projects\\Chaldal_scrapping_project\\Chaldal_scraper\\scrapped_data\\"
-file_name = "Chaldaal_rawdata_v1.1.xlsx"
+file_name = "Chaldaal_rawdata_v1.2.xlsx"
 full_filepath = path + file_name
 print(full_filepath)
 
 
 class PageHandler:
     def __init__(self):
-        pass
+        self.driver = webdriver.Chrome()
+        self.driver.get(url_home)
 
     def scroll_page(self):
         # Scroll down the page multiple times to load more content
@@ -34,6 +36,20 @@ class PageHandler:
         html_content = self.driver.page_source
         soup = BeautifulSoup(html_content, "lxml")
         return soup
+
+
+def get_url(link_container):
+    container = link_container
+    url_list = []
+    url_title = []
+    for a_tags in container:
+        links = a_tags.find_elements(By.TAG_NAME, "a")
+        for link in links:
+            url_list.append(link.get_attribute("href"))  # finds the href from the <a> tags
+            url_title.append(link.text)  # finds the link titles
+    data = {'menu': url_title, 'url': url_list}
+    df = pd.DataFrame(data)
+    return df
 
 
 class UrlManager:
@@ -60,7 +76,7 @@ class DataRetriever:
         self._get_url = get_url
 
     def find_product_description(self, product_link):
-        detail_link = self._handler.url_home + product_link
+        detail_link = url_home + product_link
         soup = self._handler.loader(detail_link)
         description = soup.find("div", class_="details").text
         return description
@@ -72,10 +88,14 @@ class DataRetriever:
         product_links = []
         descriptions = []
         category = []
+        i = 0
         try:
             productPane = soup.find("div", class_="productPane")
             products = productPane.findAll("div", class_="product")  # prod_elements == products
             for product in products:
+                if i == 2:
+                    i=0
+                    break
                 item = product.find("div", class_="name").text
                 quantity = product.find("div", class_="subText").text
                 if product.find("div", class_="discountedPrice"):
@@ -90,13 +110,14 @@ class DataRetriever:
                 description = self.find_product_description(product_link)
                 descriptions.append(description)
                 category.append(menubar)
+                i = i + 1
             df_item_by_cat = pd.DataFrame(columns=['item', 'quantity', 'price', 'description', 'category'],
                                           data={"item": items, "quantity": quantities, "price": prices,
                                                 "description": descriptions, "category": category})
             all_item.append(df_item_by_cat)
         except AttributeError:
             sub_link_container = self._handler.driver.find_elements(By.CLASS_NAME, "category-links-wrapper")
-            df_sub = self._get_url(sub_link_container)
+            df_sub = self._get_url.get_url(sub_link_container)
             submenu_list.append(df_sub)
 
     def get_data(self, df):
@@ -106,26 +127,22 @@ class DataRetriever:
 
 
 class Main:
-    url_home = "https://chaldal.com"
 
     def __init__(self, handler: PageHandler, get_url: UrlManager, get_data: DataRetriever):
-        self.driver = webdriver.Chrome()
-        self.driver.get(self.url_home)
-
         self._handler = handler
         self._get_url = get_url
         self._get_data = get_data
 
     def executor(self):
-        menu_link_container = self.driver.find_elements(By.CSS_SELECTOR, "ul[class*='level-']")
+        menu_link_container = self._handler.driver.find_elements(By.CSS_SELECTOR, "ul[class*='level-']")
         df_menu = self._get_url.get_url(menu_link_container)
         df_menu.to_excel(full_filepath, sheet_name="menu_urls", index=False)
 
         # Save the subcategories and the associated links
-        df_submenu_links = pd.concat(submenu_list, ignore_index=True)
-        print(df_menu.shape)
-        with pd.ExcelWriter(full_filepath, engine='openpyxl', mode='a') as writer:
-            df_submenu_links.to_excel(writer, sheet_name="submenu_urls", index=False)
+        # df_submenu_links = pd.concat(submenu_list, ignore_index=True)
+        # print(df_menu.shape)
+        # with pd.ExcelWriter(full_filepath, engine='openpyxl', mode='a') as writer:
+        #     df_submenu_links.to_excel(writer, sheet_name="submenu_urls", index=False)
 
         # Save information of all items in a single dataframe
         self._get_data.get_data(df_menu)
@@ -134,7 +151,7 @@ class Main:
         with pd.ExcelWriter(full_filepath, engine='openpyxl', mode='a') as writer:
             df_all_item.to_excel(writer, sheet_name="item_info", index=False)
 
-        self.driver.close()
+        self._handler.driver.close()
 
 
 
