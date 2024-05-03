b9188914fd0a7ebf189f150acb6c371f7e3027ce
IshratFarah <ishrat.tanzila.farah@gmail.com>
2024-05-03 10:44:54 +0600

    added class ChaldalAPIClient in api_client.py. added test_runner.py for executing ChaldalAPIClient.

--- Chaldal_scraper/scrapers/chaldal/chaldal_scrapper_oop.py
+++ Chaldal_scraper/scrapers/chaldal/chaldal_scrapper_oop.py
@@ -156,6 +156,12 @@ class Main:
         self._handler.driver.close()
 
 
+# code execution
+handler = PageHandler()
+get_url = UrlManager()
+get_data = DataRetriever(handler=handler,get_url=get_url)
+main = Main(handler=handler, get_url=get_url, get_data=get_data)
+main.executor()
 
 
 
